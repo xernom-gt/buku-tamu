0dbe394751b6be3e6fa62c7cef4fc9c61581f79c
xernom-gt <dasboard10@gmail.com>
2025-09-09 20:18:35 +0700

    templating app buku tamu dengan sb admin 2

--- buku-tamu.php
+++ buku-tamu.php
@@ -1,5 +1,6 @@
 <?php 
 
+require_once ('function.php');
 include_once './templates/header.php';
 
 ?>
@@ -14,7 +15,7 @@ include_once './templates/header.php';
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                            <h6 class="m-0 font-weight-bold text-primary">Buku Tamu</h6>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
@@ -40,14 +41,26 @@ include_once './templates/header.php';
                                         </tr>
                                     </tfoot>
                                     <tbody>
+                                        <?php
+                                        
+                                        $no = 1;
+
+                                        $buku_tamu = $query("SELECT * FROM buku_tamu");
+                                        foreach ($buku_tamu as $tamu) :  ?>
                                         <tr>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                        </tr>                                        
+                                            <td><?= $no++?></td>
+                                            <td><?= $tamu['tanggal']?></td>
+                                            <td><?= $tamu['nama_tamu']?></td>
+                                            <td><?= $tamu['alamat']?></td>
+                                            <td><?= $tamu['no_hp']?></td>
+                                            <td><?= $tamu['bertemu']?></td>
+                                            <td><?= $tamu['kepentingan']?></td>
+                                            <td>
+                                                <button class="btn btn-success" type="button">Ubah</button>
+                                                <button class="btn btn-danger" type="button">Hapus</button>
+                                            </td>
+                                        </tr>        
+                                        <?php endforeach; ?>                                
                                     </tbody>
                                 </table>
                             </div>
---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'root');
+define('DB_PW', '');
+define('DB_NAME', 'app_bukutamu');
+
+$conn = mysqli_connect(DB_SERVER,DB_USER,DB_PW,DB_SERVER);                  
+?>
